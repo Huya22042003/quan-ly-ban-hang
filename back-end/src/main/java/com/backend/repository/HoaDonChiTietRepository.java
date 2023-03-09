30734fc0cf315ee7837abb3a2a4290b3433acd7f
Nguyen Quoc Huy <huynqph26782@fpt.edu.vn>
2023-03-09 22:55:39 +0700

    aa

--- back-end/src/main/java/com/backend/repository/HoaDonChiTietRepository.java
+++ back-end/src/main/java/com/backend/repository/HoaDonChiTietRepository.java
@@ -3,6 +3,8 @@ package com.backend.repository;
 import com.backend.entity.HoaDonChiTiet;
 import com.backend.entity.HoaDonChiTietId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet, HoaDonChiTietId> {
 }
--- back-end/src/main/java/com/backend/repository/HoaDonRepository.java
+++ back-end/src/main/java/com/backend/repository/HoaDonRepository.java
@@ -2,6 +2,8 @@ package com.backend.repository;
 
 import com.backend.entity.HoaDon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HoaDonRepository extends JpaRepository<HoaDon, Long> {
 }
